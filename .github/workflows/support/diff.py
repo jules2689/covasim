aed0fbecd43fa0b9259260d9d213a6e0eb6ecf52
Julian Nadeau <jules2689@github.com>
2020-04-05 22:58:42 -0400

    Debug

--- .github/workflows/support/diff.py
+++ .github/workflows/support/diff.py
@@ -3,6 +3,7 @@ import os
 # Compute the diff between the cached file and our results
 stream = os.popen("diff -u .test/baselines/run_sim.json results/*.json")
 diff = stream.read()
+print(diff)
 
 # Take off the --- and +++ lines denoting the file names (the first 2 lines)
 without_file_names = diff.splitlines()[2:]
